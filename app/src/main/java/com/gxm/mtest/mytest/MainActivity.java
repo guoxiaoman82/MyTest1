8407c0dc533dc6d608772f8a4be2fd99a262af9b
guoxiaoman82 <gxmAndroid@163.com>
2019-04-28 15:25:26 +0800

    Initial commit

---
+++ app/src/main/java/com/gxm/mtest/mytest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.gxm.mtest.mytest;
+
+import android.os.Binder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Unbinder unbinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        unbinder=ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (unbinder!=null){
+            unbinder.unbind();
+        }
+    }
+}
